53e86da5c3c39b76eecb618007ecdec73a72a891
Дмитрий <firsov.2008@mail.ru>
2025-03-05 22:17:02 +0300

    4

--- internal/orchestrator/orchestrator.go
+++ internal/orchestrator/orchestrator.go
@@ -314,6 +314,9 @@ func PostResult(w http.ResponseWriter, r *http.Request) {
 					}
 				}
 			}
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(map[string]string{"status": "OK"})
 			return
 		}
 	}
